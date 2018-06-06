067942b56baf46c2a93f7327a02930391ba007c0
gooking <javazj@qq.com>
2018-06-06 08:36:39 +0800

    后台接口升级为 https

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   QQ_MAP_KEY: '"TJ3BZ-6LVCF-C25JP-JOA3O-EWZFJ-5FBMI"',
-  BASE_API: '"http://user.api.it120.cc"'
+  BASE_API: '"https://user.api.it120.cc"'
 }
