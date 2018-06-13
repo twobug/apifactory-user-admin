fcb642bc9e9e8ffe1f703fb096f1ca6c10a88c6e
Achen <chenrenjie@100tal.com>
2018-06-13 10:32:28 +0800

    add Tinymca插件 package add vuedraggable 拖动图片 功能： add 商城管理 实现增删改查

---
+++ src/api/apiExtShopGoods.js
@@ -0,0 +1,50 @@
+import request from '@/utils/request'
+
+export function fetchDataList(data) {
+  return request({
+    url: '/user/apiExtShopGoods/list',
+    method: 'post',
+    data: {...data}
+  })
+}
+
+export function getShopData() {
+  return request({
+    url: '/user/apiExtShopSub/list',
+    method: 'post',
+    data: {}
+  })
+}
+
+export function getShopGoodsCategoryData() {
+  return request({
+    url: '/user/apiExtShopGoodsCategory/list',
+    method: 'post',
+    data: {}
+  })
+
+}
+
+export function delData(id) {
+  return request({
+    url: '/user/apiExtShopGoods/del',
+    method: 'post',
+    data: { id }
+  })
+}
+
+export function infoData(id) {
+  return request({
+    url: '/user/apiExtShopGoods/info',
+    method: 'get',
+    params: { id }
+  })
+}
+
+export function saveData(data) {
+  return request({
+    url: '/user/apiExtShopGoods/save',
+    method: 'post',
+    data: { ...data }
+  })
+}
