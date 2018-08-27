d9eb38c295dc804a2f8b7af9972790dbfce28852
gooking <javazj@qq.com>
2018-08-27 15:28:45 +0800

    update webpack to v4

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,22 +10,25 @@ const service = axios.create({
 })
 
 // request拦截器
-service.interceptors.request.use(config => {
-  config.headers['X-Token'] = getToken()
-  config.data = qs.stringify(config.data)
-  return config
-}, error => {
-  // Do something with request error
-  console.log(error) // for debug
-  Promise.reject(error)
-})
+service.interceptors.request.use(
+  config => {
+    config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    config.data = qs.stringify(config.data)
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
 
 // respone拦截器
 service.interceptors.response.use(
   response => {
-  /**
-  * code为非20000是抛错 可结合自己业务进行修改
-  */
+    /**
+     * code为非20000是抛错 可结合自己业务进行修改
+     */
     const res = response.data
     if (res.code === 100000 || res.code === 100001) {
       MessageBox.confirm('登录超时，请重新登录', '确定登出', {
